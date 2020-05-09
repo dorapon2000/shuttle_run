417c60f6b0007dc5ca5f1fe3a6a97b9b57d6aff2
dorapon <dorapon2000@gmail.com>
2020-05-09 15:54:30 +0900

    Fix bug to calculate correct rate for empty problems

--- src/renderer/utils/stats.js
+++ src/renderer/utils/stats.js
@@ -1,5 +1,8 @@
 export function SumOfOkAvg (problems) {
   let result = 0.0
+  if (problems.length === 0) {
+    return
+  }
   for (let index = 0; index < problems.length; index++) {
     const problem = problems[index]
     result += OkAvg(problem)
