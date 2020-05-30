3f866be5ea40022cfc7993a22ccf09ff2fb5c89d
Masakuni Yamaoka <o1.d00m.o6@gmail.com>
2020-05-30 18:06:30 +0900

    # Create Components of Examination - this components contains bellow     - shuffle problems     - record result         - show current results     - countdown timer

---
+++ src/renderer/components/Examination.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="divExamination">
+    <header>
+      <router-link to="/">戻る</router-link>
+      <div>
+        <h1 class="title">{{book.title}}</h1>
+        <count-timer ref="examTimer" :limitOfTime="limitOfTime"></count-timer>
+      </div>
+      <h2>{{currentIndex + 1}} / {{examOrder.length}}</h2>
+      <p class='history'>
+        <span v-for='(value, index) in result' :key='index'>
+            <i class="success far fa-check-circle" v-show="value===1"></i>
+            <i class="wrong fas fa-times" v-show="value===-1"></i>
+            <i class="far fa-question-circle" v-show="!value"></i>
+        </span>
+      </p>
+    </header>
+    <div class="question">
+      {{this.nextProblem.text}}
+    </div>
+    <div class="answer">
+      <p>{{currentAnswer}}</p>
+      <button class="buttonAnswer" :disabled="!isAnswering" @click="showAnswer">答えを見る</button>
+    </div>
+    <div class="result">
+      <div class="checkAnswer success" @click="submitResult(true)"><i class="far fa-check-circle"></i></div>or
+      <div class="checkAnswer wrong" @click="submitResult(false)"><i class="fas fa-times"></i></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { shuffleArray } from '../utils/manipuleteObj'
+import CountTimer from './Timer/Timer'
+const timeoutMilliSeconds = 500
+export default {
+  name: 'examination',
+  components: {CountTimer},
+  data: function () {
+    return {
+      currentAnswer: '',
+      isAnswering: true,
+      currentIndex: 0,
+      limitOfTime: 20,
+      examOrder: [],
+      result: []
+    }
+  },
+  props: {
+    book: Object
+  },
+  computed: {
+    nextProblem: function () {
+      return this.book.problems[this.examOrder[this.currentIndex]]
+    },
+    isCorrect: function () {
+      return this.result[this.currentIndex]
+    }
+  },
+  methods: {
+    testCode: function () {
+      this.book = {
+        title: 'テスト問題',
+        problems: [
+          {id: 12, text: 'hoge', answer: 'hogehoge', stats: {OK: 0, NG: 0}},
+          {id: 13, text: 'fuga', answer: 'hogehoge', stats: {OK: 0, NG: 0}},
+          {id: 14, text: 'hogehoge', answer: 'hogehoge', stats: {OK: 0, NG: 0}},
+          {id: 15, text: 'fugafuga', answer: 'hogehoge', stats: {OK: 0, NG: 0}}
+        ]
+      }
+    },
+    showAnswer: function () {
+      this.currentAnswer = this.book.problems[this.examOrder[this.currentIndex]].answer
+    },
+    submitResult: function (result) {
+      this.result[this.currentIndex] = result ? 1 : -1
+      this.isAnswering = false
+      this.showAnswer()
+
+      if (this.currentIndex < this.result.length) {
+        setTimeout(() => {
+          this.goNext()
+        }, timeoutMilliSeconds)
+      } else {
+        this.showTotal()
+      }
+    },
+    goNext: function () {
+      this.currentAnswer = ''
+      this.currentIndex++
+      this.isAnswering = true
+      this.timer.resetCount()
+      this.timer.startCount()
+    },
+    showTotal: function () {
+      this.$router.push()
+    }
+  },
+  mounted: function () {
+    this.testCode()
+    for (let index = 0; index < this.book.problems.length; index++) {
+      this.examOrder.push(index)
+    }
+    this.examOrder = shuffleArray(this.examOrder)
+    this.result = new Array(this.book.problems.length)
+
+    this.$nextTick(function () {
+      this.timer = this.$refs.examTimer
+      this.timer.startCount()
+    })
+  }
+}
+</script>
+
+<style>
+.divExamination{
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  padding: 10px;
+  background-color: #e0ffff;
+}
+.title{
+  display: inline-block;
+  width: 50vw;
+}
+.header{
+  height: 30vh;
+}
+.question{
+  height: 50vh;
+  width: 90vw;
+  padding: 1em;
+  font-size: 2em;
+  background-color: azure;
+}
+.answer{
+  display: table-cell;
+  margin-top: 1em;
+  height: 10vh;
+  width: 90vw;
+  text-align: center;
+  vertical-align: middle;
+  background-color: lightskyblue;
+}
+.answer > p{
+  text-decoration: underline rgba(1, 1, 1, 0.5);
+  display: inline-block;
+  width: 20em;
+  font-size: 2em;
+}
+.buttonAnswer{
+  display: inline;
+  padding: 10px;
+  border-radius: 0.3em;
+  font-size: 1.5em;
+  color: whitesmoke;
+  background-color: cornflowerblue;
+}
+.result{
+  width: 100%;
+  height: 20vh;
+  top:0;
+  left: 0;
+  text-align: center;
+  font-size: 2em;
+}
+.checkAnswer{
+  display: inline-block;
+  margin: 0 1em;
+  font-size: 3em;
+}
+.checkAnswer:hover{
+  cursor: pointer;
+}
+.success{
+  color: red;
+}
+.wrong{
+  color: blue;
+}
+</style>
