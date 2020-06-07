e14b4e3472a35d8a47fe3657535bd3c6662ecb7b
Masakuni Yamaoka <o1.d00m.o6@gmail.com>
2020-06-07 15:02:01 +0900

    # Fix and Add function - fix SumOfOkAvg     - change SumOfOkAvg to SumOfAvg     - alter return values(true:OkAvg, false:NgAvg) second variable

--- src/renderer/utils/stats.js
+++ src/renderer/utils/stats.js
@@ -1,19 +1,34 @@
-export function SumOfOkAvg (problems) {
+export function SumOfAvg (problems, isRequestForOk) {
   let result = 0.0
   if (problems.length === 0) {
     return
   }
   for (let index = 0; index < problems.length; index++) {
     const problem = problems[index]
-    result += OkAvg(problem)
+    result += isRequestForOk ? OkAvg(problem) : NgAvg(problem)
   }
   return Math.round(result / problems.length)
 }
+export function SumOfOkAvg (problems) {
+  return SumOfAvg(problems, true)
+}
 
 export function OkAvg (problem) {
-  if (problem.stats.OK + problem.stats.NG === 0) {
+  if (!problem.stats.OK || !problem.stats.NG || problem.stats.OK + problem.stats.NG === 0) {
     return 0
   }
 
   return Math.ceil(problem.stats.OK / (problem.stats.OK + problem.stats.NG) * 100, -1)
+}
+
+export function SumOfNgAvg (problems) {
+  return SumOfAvg(problems, false)
+}
+
+export function NgAvg (problem) {
+  if (!problem.stats.OK || !problem.stats.NG || problem.stats.OK + problem.stats.NG === 0) {
+    return 0
+  }
+
+  return Math.ceil(problem.stats.NG / (problem.stats.OK + problem.stats.NG) * 100, -1)
 }
